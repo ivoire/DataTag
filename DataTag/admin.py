c31be2ebba1c83ccd48a5499474e24635d9748ad
Rémi Duraffort <ivoire@videolan.org>
2014-10-10 05:14:02 +0200

    Create the Media object

--- DataTag/admin.py
+++ DataTag/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from DataTag.models import Tag
+from DataTag.models import Media, Tag
 
+
+admin.site.register(Media)
 admin.site.register(Tag)
--- DataTag/models.py
+++ DataTag/models.py
@@ -1,13 +1,26 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.contrib.auth.models import Group, User
 from django.core.exceptions import ValidationError
 from django.db import models
 
 
+class Media(models.Model):
+    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    path = models.FilePathField(path=settings.MEDIA_ROOT, recursive=True,
+                                max_length=256)
+
+    def __str__(self):
+        base_length = len(settings.MEDIA_ROOT)
+        tags_str = ', '.join([tag.name for tag in self.tags.all()])
+        return "%s: %s" % (self.path[base_length:], tags_str)
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=128)
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
+    parent = models.ForeignKey('self', blank=True, null=True,
+                               related_name='children')
     groups = models.ManyToManyField(Group, blank=True, null=True)
     is_public = models.BooleanField(default=False)
 

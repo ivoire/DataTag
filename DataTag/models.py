73a23a7f4c469093ef405d4b28aebbfd713ed534
Rémi Duraffort <ivoire@videolan.org>
2014-10-10 04:42:49 +0200

    Create the Tag model

--- DataTag/models.py
+++ DataTag/models.py
@@ -1,3 +1,22 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import Group, User
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
+    groups = models.ManyToManyField(Group, blank=True, null=True)
+    is_public = models.BooleanField(default=False)
+
+    def clean(self):
+        if self.parent.pk == self.pk:
+            raise ValidationError({'parent': ['Parent cannot be self']})
+
+    def __str__(self):
+        if self.parent:
+            return "%s (%s)" % (self.name, self.parent.name)
+        else:
+            return "%s" % (self.name)

f53abf4c876e41d47c20443a154a322534ac7984
Rémi Duraffort <ivoire@videolan.org>
2014-10-11 20:44:21 +0200

    Media: generate thumbnails on the fly
    Only the small resolution is supported for the moment. More resolution will be
    added later on.

--- DataTag/views/media.py
+++ DataTag/views/media.py
@@ -10,21 +10,40 @@ from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 
 from DataTag.models import Media
+from DataTag.utils import mkdir
 
 import mimetypes
 import os
+from PIL import Image
 
 
 def media(request, path):
+    pathname = os.path.join(settings.MEDIA_ROOT, path)
     # Get the Media and check the permissions
-    media = get_object_or_404(Media, path=os.path.join(settings.MEDIA_ROOT,
-                                                       path))
+    media = get_object_or_404(Media, path=pathname)
     # TODO: check the permissions
 
+    # Get a thumbnails if requested
+    size = request.GET.get('size', None)
+    if size == 'small':
+        smallpath = os.path.join(settings.CACHE_ROOT, 'small', path)
+        if not os.path.isfile(smallpath):
+        # Create the destination directory
+            mkdir(os.path.dirname(smallpath))
+
+            # Create the thumbnail, copying the EXIF data
+            image = Image.open(pathname)
+            # TODO: what if there is no EXIF data?
+            # http://stackoverflow.com/questions/4228530/pil-thumbnail-is-rotating-my-image
+            exif = image.info['exif']
+            image.thumbnail((200,200), Image.ANTIALIAS)
+            image.save(smallpath, exif=exif)
+        pathname = smallpath
+
     # Stream the file
-    wrapper = FileWrapper(open(media.path))
-    mime = mimetypes.guess_type(media.path)
+    wrapper = FileWrapper(open(pathname))
+    mime = mimetypes.guess_type(pathname)
     response = HttpResponse(wrapper,
                             content_type=mime[0] if mime[0] else 'text/plain')
-    response['Content-Length'] = os.path.getsize(media.path)
+    response['Content-Length'] = os.path.getsize(pathname)
     return response

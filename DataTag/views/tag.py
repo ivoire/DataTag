76f9a728e3a52e2f1d9e817c1844da6f43c53ad9
Rémi Duraffort <ivoire@videolan.org>
2014-10-24 16:41:18 +0200

    Tag: only show the media not the children tags
    Children tags are available while browsing the tags tree.

--- DataTag/views/tag.py
+++ DataTag/views/tag.py
@@ -11,18 +11,12 @@ from DataTag.models import Tag
 
 def tag(request, tags):
     # TODO: check the permissions
-    if tags:
-        # Check the existence of the tags. Also check the parent relationship
-        previous = None
-        tag = None
-        for tagname in tags.split('/'):
-            tag = get_object_or_404(Tag, name=tagname, parent=tag)
-            previous = tagname
-    else:
-        # Create a dummy Tag object
-        tag = dict()
-        # TODO: only show the allowed tags!
-        tag['children'] = Tag.objects.filter(parent=None)
+    # Check the existence of the tags. Also check the parent relationship
+    previous = None
+    tag = None
+    for tagname in tags.split('/'):
+        tag = get_object_or_404(Tag, name=tagname, parent=tag)
+        previous = tagname
 
     return render_to_response('DataTag/tag/index.html', {'tag': tag},
                               context_instance=RequestContext(request))

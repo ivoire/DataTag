d33900d5a045e5092eabe91dc2c14a87ab64356e
Rémi Duraffort <ivoire@videolan.org>
2014-10-24 16:44:09 +0200

    Add two views to browse the tag tree

--- DataTag/views/main.py
+++ DataTag/views/main.py
@@ -3,10 +3,23 @@
 
 from __future__ import unicode_literals
 
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+
+from DataTag.models import Tag
 
 
 def index(request):
-    return render_to_response('DataTag/main/index.html',
+    return render_to_response('DataTag/main/index.html', {'children': Tag.objects.filter(parent=None)},
+                              context_instance=RequestContext(request))
+
+
+def browse(request, path):
+    previous = None
+    tag = None
+    for tagname in path.split('/'):
+        tag = get_object_or_404(Tag, name=tagname, parent=tag)
+        previous = tagname
+
+    return render_to_response('DataTag/main/browse.html', {'tag': tag},
                               context_instance=RequestContext(request))

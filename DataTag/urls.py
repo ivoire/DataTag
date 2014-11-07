4cd5a93a3b09019d318944778164338568499aa4
Rémi Duraffort <ivoire@videolan.org>
2014-11-07 16:51:48 +0100

    First template for the login and logged_out pages
    The links to register and reset the password are not working for the moment.

--- DataTag/urls.py
+++ DataTag/urls.py
@@ -7,14 +7,23 @@ from django.conf.urls import include, patterns, url
 from django.core.urlresolvers import reverse_lazy
 
 
+# Main view
 urlpatterns = patterns('DataTag.views.main',
     url(r'^$', 'index', name='index'),
 )
 
+# Authentication
+urlpatterns += patterns('django.contrib.auth.views',
+    url(r'^accounts/login/$', 'login', {'template_name': 'DataTag/account/login.html'}, name='accounts.login'),
+    url(r'^accounts/logout/$', 'logout', {'template_name': 'DataTag/account/logged_out.html'}, name='accounts.logout'),
+)
+
+# Medias
 urlpatterns += patterns('DataTag.views.media',
     url(r'^medias/(?P<path>.*$)', 'media', name='media'),
 )
 
+# Tags
 urlpatterns += patterns('DataTag.views.tag',
     url(r'^tags/$', 'tag', {'path': ''}, name='tags.root'),
     url(r'^tags/(?P<path>.*)/$', 'tag', name='tags.details'),

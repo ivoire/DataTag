d8ff10538b60c5bd8d4e39eacdc384518c09c560
Rémi Duraffort <ivoire@videolan.org>
2014-11-09 09:59:07 +0100

    Admin: improve display by adding more info

--- DataTag/admin.py
+++ DataTag/admin.py
@@ -5,5 +5,22 @@ from django.contrib import admin
 from DataTag.models import Media, Tag
 
 
-admin.site.register(Media)
-admin.site.register(Tag)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('path', 'tag_list')
+    list_filter = ('tags',)
+    filter_horizontal = ('tags',)
+    search_fields = ('path',)
+
+    def tag_list(self, obj):
+        return "|".join([tag.name for tag in obj.tags.all()])
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'group_list', 'is_public')
+    list_filter = ('groups',)
+    filter_horizontal = ('groups',)
+    def group_list(self, obj):
+        return "|".join([tag.name for tag in obj.groups.all()])
+
+admin.site.register(Media, MediaAdmin)
+admin.site.register(Tag, TagAdmin)

dff92ceace243e8d2d2e832fa1467352c913d93e
Rémi Duraffort <ivoire@videolan.org>
2014-12-07 10:11:17 +0100

    Profile: allow to update some information

--- DataTag/urls.py
+++ DataTag/urls.py
@@ -4,8 +4,9 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse_lazy
 
-from DataTag.views.account import DTAuthenticationForm
+from DataTag.views.account import DTAuthenticationForm, DTPasswordChangeForm
 
 # Main view
 urlpatterns = patterns('DataTag.views.main',
@@ -16,12 +17,15 @@ urlpatterns = patterns('DataTag.views.main',
 urlpatterns += patterns('django.contrib.auth.views',
     url(r'^accounts/login/$', 'login', {'template_name': 'DataTag/account/login.html', 'authentication_form': DTAuthenticationForm}, name='accounts.login'),
     url(r'^accounts/logout/$', 'logout', {'template_name': 'DataTag/account/logged_out.html'}, name='accounts.logout'),
+    url(r'^accounts/password/change/$', 'password_change', {'template_name': 'DataTag/account/password_change.html', 'password_change_form': DTPasswordChangeForm, 'post_change_redirect': reverse_lazy('accounts.password_change_done')}, name='accounts.password_change'),
 )
 
 # Account
 urlpatterns += patterns('DataTag.views.account',
     url(r'^accounts/register/$', 'register', name='accounts.register'),
     url(r'^accounts/profile/$', 'profile', name='accounts.profile'),
+    url(r'^accounts/profile/update/$', 'update', name='accounts.profile.update'),
+    url(r'^accounts/password/change/done/$', 'password_change_done', name='accounts.password_change_done'),
 )
 
 # Medias
--- DataTag/views/account.py
+++ DataTag/views/account.py
@@ -5,8 +5,11 @@ from __future__ import unicode_literals
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
@@ -26,6 +29,24 @@ class DTAuthenticationForm(AuthenticationForm):
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password'].widget.attrs['class'] = 'form-control'
+
+
+class DTPasswordChangeForm(PasswordChangeForm):
+    """
+    Override the default PasswordChangeForm in order to add HTML5 attributes.
+    This is the only change done and needed
+    """
+    def __init__(self, *args, **kwargs):
+        super(DTPasswordChangeForm, self).__init__(*args, **kwargs)
+        # Add HTML5 attributes
+        self.fields['old_password'].widget.attrs['placeholder'] = _('Old password')
+        self.fields['old_password'].widget.attrs['autofocus'] = 'autofocus'
+        self.fields['new_password1'].widget.attrs['placeholder'] = _('New password')
+        self.fields['new_password2'].widget.attrs['placeholder'] = _('New password')
+
+        self.fields['old_password'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password1'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password2'].widget.attrs['class'] = 'form-control'
 
 
 class DTUserCreationForm(UserCreationForm):
@@ -68,6 +89,21 @@ class DTUserCreationForm(UserCreationForm):
         return user
 
 
+class DTUserUpdateForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        super(DTUserUpdateForm, self).__init__(*args, **kwargs)
+        # first_name and last_name are required
+        self.fields['first_name'].required = True
+        self.fields['first_name'].widget.attrs['autofocus'] = 'autofocus'
+
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+
+
 def register(request):
     if request.method == 'POST':
         user_form = DTUserCreationForm(request.POST)
@@ -85,6 +121,29 @@ def register(request):
 
 
 @login_required
+def password_change_done(request):
+    messages.success(request, _('Password changed successfully'))
+    return HttpResponseRedirect(reverse('accounts.profile'))
+
+
+@login_required
 def profile(request):
     return render_to_response('DataTag/account/profile.html',
+                              context_instance=RequestContext(request))
+
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        user_form = DTUserUpdateForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            # Print the message
+            messages.success(request, _("Personnal information updated"))
+            return HttpResponseRedirect(reverse('accounts.profile'))
+    else:
+        user_form = DTUserUpdateForm(instance=request.user)
+
+    return render_to_response('DataTag/account/update.html',
+                              {'user_form': user_form},
                               context_instance=RequestContext(request))

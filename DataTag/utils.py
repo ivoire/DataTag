d4f4fe748630b66c5cc021e387331d99b46e9034
Rémi Duraffort <ivoire@videolan.org>
2015-02-02 21:21:16 +0100

    Move the thumbnail creation in a specific module

--- DataTag/utils.py
+++ DataTag/utils.py
@@ -19,10 +19,15 @@
 
 from __future__ import unicode_literals
 
+from django.conf import settings
+
 import errno
 import json
 import os
+from PIL import Image
+import shutil
 import subprocess
+import tempfile
 
 
 def mkdir(path):
@@ -60,3 +65,66 @@ def keep_AR(old_size, new_size):
         x = int(max(x * new_size[1] / y, 1))
         y = int(new_size[1])
     return (x, y)
+
+
+def create_thumbnail(media, dest_path, size):
+    src_path = media.path
+    # Detect the media format (image or video)
+    try:
+        image = Image.open(src_path)
+        media_format = "image"
+    except (OSError, IOError):
+        media_format = "video"
+
+    if media_format == "image":
+        exif = image._getexif()
+        if exif:
+            # Get orientation
+            orientation = exif.get(0x0112, 0)
+            if orientation == 3:
+                image = image.transpose(Image.ROTATE_180)
+            elif orientation == 6:
+                image = image.transpose(Image.ROTATE_270)
+            elif orientation == 8:
+                image = image.transpose(Image.ROTATE_90)
+
+        image.thumbnail(size, Image.ANTIALIAS)
+        image.save(dest_path)
+
+    elif media_format == "video":
+        # If the video size is known, compute the right size to keep
+        # the A/R
+        if media.width != 0 and media.height != 0:
+            size = keep_AR((media.width, media.height), size)
+
+        # Create the thumbnail in a temp directory in order to use the
+        # use the right extensions for convert. In fact, convert uses
+        # the extension to guess the input and output format. This is
+        # not working for video thumbnails because we want to keep the
+        # original filename (including the extension).
+        tmp_dir = tempfile.mkdtemp('DataTag')
+        tmp_path = os.path.join(tmp_dir, 'thumbnail.jpg')
+        try:
+            subprocess.check_output(['ffmpeg', '-i', src_path,
+                                     '-vcodec', 'mjpeg',
+                                     '-vframes', '1',
+                                     '-an', '-f', 'rawvideo',
+                                     '-s', "%dx%d" % size,
+                                     tmp_path],
+                                    stderr=subprocess.STDOUT)
+            subprocess.check_output(['convert', tmp_path,
+                                     os.path.join(settings.STATIC_ROOT,
+                                                  'DataTag', 'img',
+                                                  'overlay.png'),
+                                     '-gravity', 'center', '-composite',
+                                     '-format', 'jpg', '-quality', '90',
+                                     tmp_path],
+                                    stderr=subprocess.STDOUT)
+            # Move the file using shutil because the directories can be
+            # on different filesystems.
+            shutil.move(tmp_path, dest_path)
+        except Exception:
+            return False
+        finally:
+            shutil.rmtree(tmp_dir)
+    return True
--- DataTag/views/media.py
+++ DataTag/views/media.py
@@ -25,14 +25,10 @@ from django.http import Http404, HttpResponseForbidden, StreamingHttpResponse
 from django.shortcuts import get_object_or_404
 
 from DataTag.models import Media
-from DataTag.utils import mkdir, keep_AR
+from DataTag.utils import create_thumbnail, keep_AR, mkdir
 
 import mimetypes
 import os
-import shutil
-import subprocess
-import tempfile
-from PIL import Image
 
 
 def media(request, path):
@@ -53,68 +49,10 @@ def media(request, path):
 
         smallpath = os.path.join(settings.CACHE_ROOT, size_str, path)
         if not os.path.isfile(smallpath):
-            # Create the destination directory
+            # Create the destination directory and thumbnail
             mkdir(os.path.dirname(smallpath))
-
-            # Detect the media format (image or video)
-            try:
-                image = Image.open(pathname)
-                media_format = "image"
-            except (OSError, IOError):
-                media_format = "video"
-
-            if media_format == "image":
-                exif = image._getexif()
-                if exif:
-                    # Get orientation
-                    orientation = exif.get(0x0112, 0)
-                    if orientation == 3:
-                        image = image.transpose(Image.ROTATE_180)
-                    elif orientation == 6:
-                        image = image.transpose(Image.ROTATE_270)
-                    elif orientation == 8:
-                        image = image.transpose(Image.ROTATE_90)
-
-                image.thumbnail(size, Image.ANTIALIAS)
-                image.save(smallpath)
-
-            elif media_format == "video":
-                # If the video size is known, compute the right size to keep
-                # the A/R
-                if media.width != 0 and media.height != 0:
-                    size = keep_AR((media.width, media.height), size)
-
-                # Create the thumbnail in a temp directory in order to use the
-                # use the right extensions for convert. In fact, convert uses
-                # the extension to guess the input and output format. This is
-                # not working for video thumbnails because we want to keep the
-                # original filename (including the extension).
-                tmp_dir = tempfile.mkdtemp('DataTag')
-                tmp_path = os.path.join(tmp_dir, 'thumbnail.jpg')
-                try:
-                    subprocess.check_output(['ffmpeg', '-i', pathname,
-                                             '-vcodec', 'mjpeg',
-                                             '-vframes', '1',
-                                             '-an', '-f', 'rawvideo',
-                                             '-s', "%dx%d" % size,
-                                             tmp_path],
-                                            stderr=subprocess.STDOUT)
-                    subprocess.check_output(['convert', tmp_path,
-                                             os.path.join(settings.STATIC_ROOT,
-                                                          'DataTag', 'img',
-                                                          'overlay.png'),
-                                             '-gravity', 'center', '-composite',
-                                             '-format', 'jpg', '-quality', '90',
-                                             tmp_path],
-                                            stderr=subprocess.STDOUT)
-                    # Move the file using shutil because the directories can be
-                    # on different filesystems.
-                    shutil.move(tmp_path, smallpath)
-                except Exception:
-                    raise Http404
-                finally:
-                    shutil.rmtree(tmp_dir)
-
+            if not create_thumbnail(media, smallpath, size):
+                raise Http404
         pathname = smallpath
 
     # Stream the file
